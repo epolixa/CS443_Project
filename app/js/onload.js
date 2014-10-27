171ab0c038a4ce765bc8211f8f3246c4425e6846
Erich Polixa <epolixa@gmail.com>
2014-10-27 18:51:03 -0400

    HTML/CSS shell in home.html

--- app/js/onload.js
+++ app/js/onload.js
@@ -2,22 +2,4 @@
 
 $(document).ready(function() {
 
-	/* Load header HTML */
-	$.ajax({
-	  	url: "linked/header.html",
-	  	dataType: "html",
-	  	success: 	function(data) {
-						$('body').html(data);
-					}
-	});
-
-	/* Load footer HTML */
-	$.ajax({
-	  	url: "linked/footer.html",
-	  	dataType: "html",
-	  	success: 	function(data) {
-						$('body').html(data);
-					}
-	});
-
 });
